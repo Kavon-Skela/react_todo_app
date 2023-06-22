3c1e0f5c0e1d2f3bd3b144bda7e49a099a78e8c9
Aleksandr <alexnovaklawyer@gmail.com>
2023-06-22 17:35:15 +0300

    react-todo-app-uploaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,63 @@
-import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-}
-
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+} from 'react';
+import { getAllTodos } from './api/todos';
+import { TodoContext } from './TodoContext/TodoContext';
+import { UserWarning } from './UserWarning';
+import { TodoError } from './Components/TodoError';
+import { TodosFooter } from './Components/TodosFooter';
+import { USER_ID } from './utils/globalConst';
+import { AddTodoForm } from './Components/AddTodoForm';
+import { TodoList } from './Components/TodoList';
 
 export const App: React.FC = () => {
+  const {
+    setTodos,
+    setEditTodoTitle,
+    setEditingTodoId,
+  } = useContext(TodoContext);
+
+  useEffect(() => {
+    getAllTodos(USER_ID)
+      .then(setTodos);
+  }, []);
+
+  const handleEscKeyUp = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        setEditTodoTitle('');
+        setEditingTodoId(null);
+      }
+    }, [],
+  );
+
+  useEffect(() => {
+    document.addEventListener('keyup', handleEscKeyUp);
+
+    return () => {
+      document.removeEventListener('keyup', handleEscKeyUp);
+    };
+  }, []);
+
+  if (!USER_ID) {
+    return <UserWarning />;
+  }
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
+
+      <div className="todoapp__content">
+        <AddTodoForm />
+
         <TodoList />
-      </Provider>
+
+        <TodosFooter />
+      </div>
+
+      <TodoError />
     </div>
   );
 };
